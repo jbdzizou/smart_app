930645dc83f5321f722509b3e8646aea04eef0a6
jbdzizou <jbdzizou@gmail.com>
2019-09-17 18:58:05 +0900

    modified Tag and View

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # coding: utf-8
-if Rails.env == 'development'
-  # 5.times do |i|
-  #   Message.create(user_id: 1,title: "スベらない話 Part#{i+1}", description: "本文 Part#{i+1}")
-  # end
+<%# if Rails.env == 'development'
+   20.times do |i|
+     Message.create(user_id: 1,title: "スベらない話 Part#{i+1}", description: "本文 Part#{i+1}")
+   end
 
-  # Tag.create([
-  #   { name: 'Ruby' },
-  # ])
+  Tag.create([
+    { name: 'スポーツ' },
+    { name: 'エンタメ' },
+    { name: '政治' },
+    { name: '経済' },
+    { name: 'テクノロジー' }
+  ])
 
 end
+%>
